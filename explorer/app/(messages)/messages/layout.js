8447425478d9fa89da356840be95d3037f72a7d1
Biru C. Sainju <biru.sainju@ibriz.ai>
2025-08-22 08:56:08 +0545

    fix: added new ico and link previews

--- explorer/app/(messages)/messages/layout.js
+++ explorer/app/(messages)/messages/layout.js
@@ -7,7 +7,29 @@ import Footer from '@/components/footer'
 export const metadata = {
     title: `SODAXScan · Fast and reliable message tracking`,
     description: `SODAXScan tracks SODAX transactions across blockchain networks.`,
-    icons: [{ rel: 'icon', url: Favicon.src }]
+    icons: [{ rel: 'icon', url: Favicon.src }],
+    openGraph: {
+        title: "SODAXScan · Fast and reliable message tracking",
+        description: "SODAXScan tracks SODAX transactions across blockchain networks.",
+        url: "https://sodaxscan.com",
+        siteName: "Sodax Scan",
+        images: [
+            {
+                url: "https://sodaxscan.com/images/link-preview.png",
+                width: 1200,
+                height: 630,
+                alt: "Sodax Scan Preview",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Sodax Scan",
+        description: "SODAXScan tracks SODAX transactions across blockchain networks.",
+        images: ["https://sodaxscan.com/images/link-preview.png"],
+    },
 }
 
 export default function MessageLayout({ children }) {
